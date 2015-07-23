52784cfe4ced85c58fa04aa5d02da058381bc98e
Luke Lee <samsung3star@gmail.com>
2015-07-23 17:22:04 -0400

    m

---
+++ app/src/main/java/lukesterlee/c4q/nyc/memegenerator/EditorActivity.java
@@ -0,0 +1,20 @@
+package lukesterlee.c4q.nyc.memegenerator;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Luke on 6/11/2015.
+ */
+public class EditorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
